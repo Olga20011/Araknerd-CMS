ce362bdae559e4a8f29a2bb6e4b7e0dd6e2c61cd
Olga20011 <olgaestherakello@gmail.com>
2024-02-14 15:01:46 +0300

    Araknerd-files

---
+++ App.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * THIS IS THE APPLICATION ROUTER
+ * 
+ * ALL ENDPOINTS SHOULD BE INCLUDED IN THE api FOLDER
+ * 
+ * Depending on the size of your application, you can keep this file organized by just including routes from another directory.
+ */
+include_once "routes/route_user.php";
+
+?>
